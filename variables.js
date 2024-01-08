e33b60dc1da6e348b2f63826912a3855780a6e49
Javeria29 <javeriaafsar298pafkiet@gmail.com>
2024-01-08 09:38:59 +0500

    Variables

---
+++ variables.js
@@ -0,0 +1,15 @@
+const UserId=1234
+let Address="12wephase"
+var age=12
+City="karachi"; /* dont use this method to declare varibales */
+
+let Email;
+// UserId=342 we cannot update value of constant variables 
+
+
+
+// prefer not to use Var due to block scope and functional scope 
+Address="DHA Phase-II"
+age=22
+City="lahore"
+console.table([UserId,Address,age,City,Email])
