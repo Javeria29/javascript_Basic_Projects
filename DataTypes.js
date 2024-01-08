5b53f578199b5f62d9592ac3a68302090be8b36f
Javeria29 <javeriaafsar298pafkiet@gmail.com>
2024-01-08 10:15:56 +0500

    Datatypes

---
+++ DataTypes.js
@@ -0,0 +1,15 @@
+"use strict" //used for new ersion of javascript 
+
+let RollNo=123 //number type
+console.log(typeof RollNo);
+
+
+let Name="Javeria" //string type
+console.log(typeof Name);
+
+let State=null //null is a standalone value ----also a object
+console.log(typeof State);
+
+let ServerResponse=true //bool type
+console.log(typeof ServerResponse);
+
